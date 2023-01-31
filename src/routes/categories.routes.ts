9100b990a75ac5e301a65500dc262d07797a8a8c
Danilo Martins <danfonmar@gmail.com>
2023-01-31 15:34:05 -0300

    debudando aplicacao, criando categoria, inserindo Id com uuid, inserindo tipagem para categoria, criando repositorio de categoria, listando as categorias, validando cadastro de categoria

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,17 +1,27 @@
 import { Router } from "express"
 
-const categoriesRoutes = Router()
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
 
-const categories = []
+const categoriesRoutes = Router();
+const categoriesRepository = new CategoriesRepository();
 
-categoriesRoutes.post("/categories", (request, response) => {
+categoriesRoutes.post("/", (request, response) => {
     const { name, description } = request.body
 
-    categories.push({
-        name,
-        description,
-    })
+    const categoryAlreadyExists = categoriesRepository.findByName(name);
+    if(categoryAlreadyExists) {
+        return response.status(400).json({ error: "Category already exists" })
+    }
+
+    categoriesRepository.create({name, description});
+
     return response.status(201).send()
 });
+
+categoriesRoutes.get("/", (request, response) => {
+    const all = categoriesRepository.list();
+
+    return response.json(all)
+})
 
 export { categoriesRoutes };
