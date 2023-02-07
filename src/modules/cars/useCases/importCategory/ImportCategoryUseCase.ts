e9d2b323bab3a993d328e04dd6fb7fe8014cfc23
Danilo Martins <danfonmar@gmail.com>
2023-02-07 16:33:46 -0300

    sawagger setup

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,18 +1,57 @@
 import {parse as csvParse } from "csv-parse";
 import fs from "fs";
+import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
+
+interface IImportCategory{
+    name: string;
+    description: string;
+}
 
 class ImportCategoryUseCase {
-    execute(file: Express.Multer.File): void {
-       const stream = fs.createReadStream(file.path)
+    constructor(private categoryRepository: ICategoriesRepository) {}
+
+    loadCategory(file: Express.Multer.File): Promise<IImportCategory[]>{
+        return new Promise((resolve, reject) => {
+            const stream = fs.createReadStream(file.path)
+            const categories: IImportCategory[] = [];
       
-       const parseFile = csvParse(); 
+            const parseFile = csvParse(); 
 
-       stream.pipe(parseFile)
+            stream.pipe(parseFile)
 
-       parseFile.on("data", async (line) => {
-        console.log(line);
-        
+            parseFile.on("data", async (line) => {
+                const [name, description] = line;
+
+                categories.push({
+                    name,
+                    description
+                });
+            })
+            .on("end", () => {
+                resolve(categories)
+            })
+            .on("error", (err) => {
+                reject(err)
+            });
+        });
+    }
+
+    async execute(file: Express.Multer.File): Promise<void> {
+       const categories = await this.loadCategory(file);
+
+       categories.map((category) => {
+        const {name, description } = category;
+
+        const existCategory = this.categoryRepository.findByName(name);
+
+        if(!existCategory){
+            this.categoryRepository.create({
+                name,
+                description
+            });
+        }
        });
     }
 }
+
 export { ImportCategoryUseCase }
--- src/modules/cars/useCases/importCategory/index.ts
+++ src/modules/cars/useCases/importCategory/index.ts
@@ -1,9 +1,9 @@
+import { CategoriesRepository } from "../../repositories/implementation/CategoriesRepository";
 import { ImportCategoryController } from "./ImportCategoryController";
 import { ImportCategoryUseCase } from "./ImportCategoryUseCase";
 
-
-const importCategoryUseCase = new ImportCategoryUseCase();
-
+const categoriesRepoitory = CategoriesRepository.getInstace();
+const importCategoryUseCase = new ImportCategoryUseCase(categoriesRepoitory);
 const importCategoryController = new ImportCategoryController(importCategoryUseCase);
 
 export{ importCategoryController }
